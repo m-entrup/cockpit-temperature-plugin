2e186efb134a68aacc73e5c3c2a79208c7edce74
Michael Entrup (b. Epping) <mail@m-entrup.de>
2021-05-06 19:32:46 +0200

    Using sensors to get temperature and monitor 2 cores.
    The regular expression is changed to parse data like this:
    ```
    coretemp-isa-0000
    Adapter: ISA adapter
    Package id 0:  +35.0°C  (high = +80.0°C, crit = +100.0°C)
    Core 0:        +34.0°C  (high = +80.0°C, crit = +100.0°C)
    Core 1:        +32.0°C  (high = +80.0°C, crit = +100.0°C)
    ```
    In this case `pt1` will be 34.0 and `pt2` 32.0.

--- temperature.js
+++ temperature.js
@@ -3,29 +3,42 @@ window.onload = function () {
 
     var chart = new SmoothieChart({millisPerPixel: 200, maxValueScale: 1.1, minValueScale: 1.1, scaleSmoothing: 0.1, grid: { fillStyle: '#ffffff', millisPerLine: 5000, verticalSections: 8 }, labels: { fillStyle: '#0000ff', fontSize: 18, precision: 1 }, timestampFormatter: SmoothieChart.timeFormatter }),
         canvas = document.getElementById('smoothie-chart'),
-        series = new TimeSeries();
-        average_array = []
+        series1 = new TimeSeries(),
+        series2 = new TimeSeries(),
+        average_array1 = [],
+        average_array2 = [];
 
-    chart.addTimeSeries(series, { lineWidth: 3.5, strokeStyle: '#ff0000' });
+    chart.addTimeSeries(series1, { lineWidth: 3.5, strokeStyle: '#ff0000' });
+    chart.addTimeSeries(series2, { lineWidth: 3.5, strokeStyle: '#ff00ff' });
     chart.streamTo(canvas, 500);
 
-    function run_proc(series,average_array) {
-        var proc = cockpit.spawn(["vcgencmd","measure_temp"]);
+    function run_proc(series1, series2, average_array1, average_array2) {
+        var proc = cockpit.spawn("sensors");
+        // var proc = cockpit.spawn(["vcgencmd","measure_temp"]);
         proc.done(function(data){
-            pt = parseFloat(data.match(/([0-9\.]+)/)[1]);
-            series.append(new Date().getTime(), pt);
-            document.getElementById("cur_temp").innerHTML = pt;
-            average_array.push(pt);
-            if(average_array.length > 5*60){
-                average_array.shift();
+            var time = new Date().getTime(),
+                pt1 = parseFloat(data.match(/Core 0:\s+\+([0-9\.]+)/)[1]),
+                pt2 = parseFloat(data.match(/Core 1:\s+\+([0-9\.]+)/)[1]);
+            series2.append(time, pt1);
+            series1.append(time, pt2);
+            document.getElementById("cur_temp1").innerHTML = pt1;
+            document.getElementById("cur_temp2").innerHTML = pt2;
+            average_array1.push(pt1);
+            if(average_array1.length > 5*60){
+                average_array1.shift();
             }
-            document.getElementById("avg_temp").innerHTML = average(average_array);
-            console.log(average_array);
+            average_array2.push(pt2);
+            if(average_array2.length > 5*60){
+                average_array2.shift();
+            }
+            document.getElementById("avg_temp1").innerHTML = average(average_array1);
+            document.getElementById("avg_temp2").innerHTML = average(average_array2);
+            console.log(average_array1, average_array2);
         });
     };
 
     //run_proc(series)
-    setInterval(function () { run_proc(series,average_array) }, 1000);
+    setInterval(function () { run_proc(series1, series2, average_array1, average_array2) }, 1000);
 }
 
 function resize_canvas() {
